ee8ea58dd8f5a7a547c94767863aaae37f6f47f1
Timotius Oktorio <timotiusoktorio@gmail.com>
2016-03-23 17:03:33 -0400

    Version 1.0.1 - Fetch movies data from TMDB API in a background thread

---
+++ app/src/main/java/com/timotiusoktorio/popularmovies/Constants.java
@@ -0,0 +1,12 @@
+package com.timotiusoktorio.popularmovies;
+
+/**
+ * Created by Timotius on 2016-03-23.
+ */
+
+public class Constants {
+
+    public static final String TMDB_BASE_URL = "http://api.themoviedb.org/3/movie/";
+    public static final String PARAM_API_KEY = "api_key";
+
+}
--- app/src/main/java/com/timotiusoktorio/popularmovies/activities/MainActivity.java
+++ app/src/main/java/com/timotiusoktorio/popularmovies/activities/MainActivity.java
@@ -1,7 +1,9 @@
-package com.timotiusoktorio.popularmovies;
+package com.timotiusoktorio.popularmovies.activities;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.timotiusoktorio.popularmovies.R;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,9 +11,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        String tmdbApiKey = BuildConfig.TMDB_API_KEY;
-        System.out.println("TMDB Api Key: " + tmdbApiKey);
     }
 
 }
